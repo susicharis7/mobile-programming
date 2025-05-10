a27be57a9dab05fcceeb3348f5dc0cd3a1f4fc8d
susicharis7 <susicharis99@gmail.com>
2025-05-10 11:54:09 +0200

    MainActivity Updated

--- app/src/main/java/com/example/studyflow/MainActivity.kt
+++ app/src/main/java/com/example/studyflow/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.studyflow.screens.LoginScreen
 import com.example.studyflow.ui.theme.StudyFlowTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StudyFlowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StudyFlowTheme {
-        Greeting("Android")
-    }
-}
