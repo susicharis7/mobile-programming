3521af4fa5ddff7a4432b6b5d2fb0b86c0aa14ab
susicharis7 <susicharis99@gmail.com>
2025-05-10 12:59:38 +0200

    App Navigation for now

--- app/src/main/java/com/example/studyflow/AppNavigation.kt
+++ app/src/main/java/com/example/studyflow/AppNavigation.kt
@@ -1,4 +1,34 @@
 package com.example.studyflow
 
-class AppNavigation {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.studyflow.screens.LoginScreen
+import com.example.studyflow.screens.RegisterScreen
+
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "register"
+    ) {
+        composable("register") {
+            RegisterScreen(
+                onLoginClick = {
+                    navController.navigate("login")
+                }
+            )
+        }
+        composable("login") {
+            LoginScreen(
+                onRegisterClick = {
+                    navController.navigate("register")
+                }
+            )
+        }
+    }
 }
