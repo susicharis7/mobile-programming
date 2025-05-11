689f32bd395c9975e51ce408554996d20395b836
saninzajmovic <sanin.zajmovic@stu.ibu.edu.ba>
2025-05-11 22:07:27 +0200

    minor update
    just changed letterSpacing for Dashboard title text

--- app/src/main/java/com/example/studyflow/screens/Dashboard.kt
+++ app/src/main/java/com/example/studyflow/screens/Dashboard.kt
@@ -28,6 +28,7 @@ import androidx.compose.material.icons.filled.Schedule
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.em
 
 @Composable
 fun DashboardScreen() {
@@ -58,7 +59,7 @@ fun DashboardScreen() {
                     color = TextWhite,
                     fontSize = 26.sp,
                     fontWeight = FontWeight.Normal,
-                    letterSpacing = 2.sp
+                    letterSpacing = 0.05.em
                 )
 
                 Icon(
