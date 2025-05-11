df0084c32dafacf3f4ca02b5e7e63cf68d761131
saninzajmovic <sanin.zajmovic@stu.ibu.edu.ba>
2025-05-12 00:28:10 +0200

    Added screen skeletons

---
+++ app/src/main/java/com/example/studyflow/screens/PomodoroTimer.kt
@@ -0,0 +1,70 @@
+package com.example.studyflow.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import com.example.studyflow.R
+import com.example.studyflow.ui.theme.BackgroundColor
+import com.example.studyflow.ui.theme.TextWhite
+
+@Composable
+fun PomodoroTimer() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundColor)
+            .padding(14.dp)
+    ) {
+        item {  // Top Bar - Icons, Dashboard
+            Spacer(modifier = Modifier.height(19.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Icon(
+                    Icons.Default.Menu,
+                    contentDescription = null,
+                    tint = TextWhite,
+                    modifier = Modifier.size(34.dp)
+                )
+
+                Text(
+                    "Study Timer",
+                    color = TextWhite,
+                    fontSize = 26.sp,
+                    fontWeight = FontWeight.Normal,
+                    letterSpacing = 0.05.em
+                )
+
+                Icon(
+                    painter = painterResource(id = R.drawable.headphones),
+                    contentDescription = null,
+                    tint = TextWhite,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(46.dp))
+        }
+    }
+}
