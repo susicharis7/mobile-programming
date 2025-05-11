a8fa2bc798979a2b3746bf697655ed12b350ef94
susicharis7 <susicharis99@gmail.com>
2025-05-11 18:03:39 +0200

    Added new colors, organized the file

--- app/src/main/java/com/example/studyflow/ui/theme/Color.kt
+++ app/src/main/java/com/example/studyflow/ui/theme/Color.kt
@@ -1,25 +1,37 @@
 package com.example.studyflow.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
+// Tasks Completed
+val TaskCompletedGradientColor = Brush.linearGradient(colors = listOf(Color(0xFF6366F1), Color(0xFF393B8B )))
+val TaskCompletedNumber = Color(0xFF818CF8)
+// Current Session
+val CurrentSessionHours = Color(0xFFC084FC)
+val CurrentSessionGradientColor = Brush.linearGradient(colors = listOf(Color(0xFFC084FC), Color(0xFF724F96)))
+// Average Study Time
+val AVGStudyTimeHours = Color(0xFF14B8A6)
+val AVGStudyTimeGradientColor = Brush.linearGradient(colors = listOf(Color(0xFF14B8A6), Color(0xFF09524A)))
+// Streak
+val StreakColor = Color(0xFFF59E0B)
 
-
+// Additional Colors
 val GreenColor = Color(0xFF00E676)
 val LoginGreen = Color(0xFF48CB94)
 val ButtonGradientColor = listOf(Color(0xFF48CB94), Color(0xFF2F9168))
 val BackgroundColor = Color(0xFF0E1D25)
 val CardBackgroundColor = Color(0xED183040)
 val TextWhite = Color(0xFFFAFAFA)
-val TextGray = Color(0xFFA0A0A0)
+val UpcomingTasksBackground = Color(0xFF374151)
 val TextBlack = Color(0xFF171717)
-// Add more colors as needed
 
 
 
+
+// Default Colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
