955d0a592dec3dba2fc66fe2fe0b9cabadbedab1
saninzajmovic <sanin.zajmovic@stu.ibu.edu.ba>
2025-05-12 21:33:27 +0200

    dependency that might be used later

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
